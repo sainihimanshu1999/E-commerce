7ec9812f37b54df1bf3d91d6508fcb12aea22846
Himanshu Saini <sainihimanshu.1999@gmail.com>
2020-03-24 12:58:29 +0530

    adding new models

--- src/core/urls.py
+++ src/core/urls.py
@@ -4,5 +4,8 @@ from .views import item_list
 app_name = 'core'
 
 urlpatterns = [
-    path('', item_list, name = 'item-list')
+    path('', item_list, name = 'item-list'),
+    
+
+
 ]
--- src/core/views.py
+++ src/core/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import render
 from .models import Item
 
-def item_list(request):
+def products(request):
     context = {
         'items' : Item.objects.all()
     }
-    return render(request, 'item_list.html' , context)
+    return render(request, "products.html" , context)
+
+
+def checkout(request):
+    return render(request, "checkout.html")
+
+
+def home(request):
+    return render(request, "home.html")
+
+
+
+
 
