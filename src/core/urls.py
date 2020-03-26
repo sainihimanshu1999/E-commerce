71654fce4861e74454f64f3cc9616262e0f5c5f9
Himanshu Saini <sainihimanshu.1999@gmail.com>
2020-03-26 14:44:23 +0530

    templates done

--- src/core/urls.py
+++ src/core/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
-from .views import item_list
+from .views import HomeView
+from .views import checkout
+from .views import product
 
 app_name = 'core'
 
 urlpatterns = [
-    path('', item_list, name = 'item-list'),
+    path('', HomeView.as_view() , name = "home"),
+    path('checkout/', checkout , name = "checkout"),
+    path('products.html', product , name = "product"),
     
 
 
--- src/core/views.py
+++ src/core/views.py
@@ -1,19 +1,36 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 from .models import Item
 
-def products(request):
+
+
+class HomeView(ListView):
+    model = Item
+    template_name = "home.html"
+
+
+class ItemDetailView(DetailView):
+    model = Item
+    template_name = "product.html"
+
+
+def checkout(request):
+    context = {
+        'items' : Item.objects.all()
+    }
+    return render(request, "checkout.html", context )
+
+
+
+def product(request):
     context = {
         'items' : Item.objects.all()
     }
     return render(request, "products.html" , context)
 
 
-def checkout(request):
-    return render(request, "checkout.html")
 
 
-def home(request):
-    return render(request, "home.html")
 
 
 
