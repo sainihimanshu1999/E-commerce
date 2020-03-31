517df30938abda6a937cb2d8623095b8f0f8fa18
Himanshu Saini <sainihimanshu.1999@gmail.com>
2020-03-31 13:54:34 +0530

    add to cart fucntionality

--- src/core/urls.py
+++ src/core/urls.py
@@ -1,14 +1,16 @@
 from django.urls import path
 from .views import HomeView
 from .views import checkout
-from .views import products
+from .views import ItemDetailView
+from .views import add_to_cart
 
 app_name = 'core'
 
 urlpatterns = [
-    path('', HomeView.as_view() , name = "home"),
-    path('checkout/', checkout , name = "checkout"),
-    path('products/', products, name = "products"),
+    path('', HomeView.as_view() , name = 'home'),
+    path('checkout/', checkout , name = 'checkout'),
+    path('products/<slug>/', ItemDetailView.as_view(), name = 'products'),
+    path('add-to-cart/<slug>/', add_to_cart, name = 'add-to-cart'),
     
 
 
--- src/core/views.py
+++ src/core/views.py
@@ -1,12 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView, DetailView
-from .models import Item
+from django.utils import timezone
+from .models import Item, OrderItem, Order
 
 
 
 class HomeView(ListView):
     model = Item
     template_name = "home.html"
+
+
+class ItemDetailView(DetailView):
+    model = Item
+    template_name = "products.html"
 
 
 
@@ -20,11 +26,28 @@ def checkout(request):
 
 
 
-def products(request):
-    context = {
-        'items' : Item.objects.all()
-    }
-    return render(request, "products.html" , context)
+def add_to_cart(request, slug):
+    item = get_object_or_404(Item , slug=slug)
+    order_item = OrderItem.objects.create(item=item)
+    order_qs = Order.objects.filter(user=request.user, ordered=False)
+    if order_qs.exists():
+        order = order_qs[0]
+        #checking wheter the ordern item is in the order
+        if order.items.filter(item__slug=item.slug).exists():
+            order_item.quantity +=1
+            order_item.save()
+    else:
+        ordered_date = timezone.now()
+        order = Order.objects.create(user = request.user, ordered_date = ordered_date)
+        order.items.add(order_item)
+    return redirect("core:products" , slug=slug)
+
+
+
+
+
+
+
 
 
 
